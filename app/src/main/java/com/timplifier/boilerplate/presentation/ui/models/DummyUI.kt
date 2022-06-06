7b1fe46627a63492348db96681569f7bb5ca79a8
Clar1fy <timplifier@gmail.com>
2022-06-07 01:59:36 +0600

    feat: add firebase dependencies to version catalog, including new bundles with crashlytics and with no crashlytics

--- app/src/main/java/com/timplifier/boilerplate/presentation/ui/models/DummyUI.kt
+++ app/src/main/java/com/timplifier/boilerplate/presentation/ui/models/DummyUI.kt
@@ -1,9 +1,10 @@
 package com.timplifier.boilerplate.presentation.ui.models
 
 import com.example.boilerplate.domain.models.DummyModel
+import com.timplifier.boilerplate.presentation.base.BaseDiffModel
 
 data class DummyUI(
-    val id: Any
-)
+    override val id: Any
+) : BaseDiffModel
 
 fun DummyModel.toUI() = DummyUI(id)
